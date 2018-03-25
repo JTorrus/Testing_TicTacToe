df87c0be4ade1b6cf4fa94a7dec4a2b0363a7003
JTorrus <jtorrusgalindo@gmail.com>
2018-03-26 00:22:14 +0200

    Game test finished

--- main/test_pkg/Game.java
+++ main/test_pkg/Game.java
@@ -8,4 +8,34 @@ public class Game {
             return 'O';
         }
     }
+
+    public boolean checkWinner(Board board) {
+        if (gameHasFinished(board)) {
+            return (board.getCells()[0][0].getChip() == 'X' && board.getCells()[0][1].getChip() == 'X' && board.getCells()[0][2].getChip() == 'X' ||
+                    board.getCells()[1][0].getChip() == 'X' && board.getCells()[1][1].getChip() == 'X' && board.getCells()[1][2].getChip() == 'X' ||
+                    board.getCells()[2][0].getChip() == 'X' && board.getCells()[2][1].getChip() == 'X' && board.getCells()[2][2].getChip() == 'X') ||
+                    (board.getCells()[0][0].getChip() == 'X' && board.getCells()[1][0].getChip() == 'X' && board.getCells()[2][0].getChip() == 'X' ||
+                            board.getCells()[0][1].getChip() == 'X' && board.getCells()[1][1].getChip() == 'X' && board.getCells()[2][1].getChip() == 'X' ||
+                            board.getCells()[0][2].getChip() == 'X' && board.getCells()[1][2].getChip() == 'X' && board.getCells()[2][2].getChip() == 'X') ||
+                    (board.getCells()[0][0].getChip() == 'X' && board.getCells()[1][1].getChip() == 'X' && board.getCells()[2][2].getChip() == 'X') ||
+                    (board.getCells()[2][0].getChip() == 'X' && board.getCells()[1][1].getChip() == 'X' && board.getCells()[0][2].getChip() == 'X');
+        } else {
+            return false;
+        }
+
+    }
+
+    public boolean gameHasFinished(Board board) {
+        int counter = 0;
+
+        for (int i = 0; i < board.getCells().length; i++) {
+            for (int j = 0; j < board.getCells()[0].length; j++) {
+                if (board.getCells()[i][j].getChip() != ' ') {
+                    counter++;
+                }
+            }
+        }
+
+        return counter == board.getCells().length * board.getCells().length;
+    }
 }
---
+++ test/test_pkg/GameTest.java
@@ -0,0 +1,110 @@
+package test_pkg;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.mockito.Mockito.*;
+
+public class GameTest {
+    @Test
+    public void checkWinnerWithCellsInColumnWorks() {
+        Position positionUnderTest = mock(Position.class);
+        when(positionUnderTest.getPosX()).thenReturn(1);
+        when(positionUnderTest.getPosY()).thenReturn(0);
+
+        Board boardUnderTest = new Board();
+        Game gameUnderTest = new Game();
+
+        for (int i = 0; i < boardUnderTest.getCells().length; i++) {
+            for (int j = 0; j < boardUnderTest.getCells()[0].length; j++) {
+                boardUnderTest.getCells()[i][j] = new Cell(' ');
+            }
+        }
+
+        boardUnderTest.getCells()[positionUnderTest.getPosX()][positionUnderTest.getPosY()].setChip('X');
+        boardUnderTest.getCells()[positionUnderTest.getPosX() + 1][positionUnderTest.getPosY()].setChip('X');
+        boardUnderTest.getCells()[positionUnderTest.getPosX() - 1][positionUnderTest.getPosY()].setChip('X');
+
+        Assert.assertTrue(gameUnderTest.checkWinner(boardUnderTest));
+    }
+
+    @Test
+    public void checkWinnerWithCellsInRowWorks() {
+        Position positionUnderTest = mock(Position.class);
+        when(positionUnderTest.getPosX()).thenReturn(0);
+        when(positionUnderTest.getPosY()).thenReturn(1);
+
+        Board boardUnderTest = new Board();
+        Game gameUnderTest = new Game();
+
+        for (int i = 0; i < boardUnderTest.getCells().length; i++) {
+            for (int j = 0; j < boardUnderTest.getCells()[0].length; j++) {
+                boardUnderTest.getCells()[i][j] = new Cell(' ');
+            }
+        }
+
+        boardUnderTest.getCells()[positionUnderTest.getPosX()][positionUnderTest.getPosY()].setChip('X');
+        boardUnderTest.getCells()[positionUnderTest.getPosX()][positionUnderTest.getPosY() + 1].setChip('X');
+        boardUnderTest.getCells()[positionUnderTest.getPosX()][positionUnderTest.getPosY() - 1].setChip('X');
+
+        Assert.assertTrue(gameUnderTest.checkWinner(boardUnderTest));
+    }
+
+    @Test
+    public void checkWinnerWithCellsInDiagonalWorks() {
+        Position positionUnderTest = mock(Position.class);
+        when(positionUnderTest.getPosX()).thenReturn(1);
+        when(positionUnderTest.getPosY()).thenReturn(1);
+
+        Board boardUnderTest = new Board();
+        Game gameUnderTest = new Game();
+
+        for (int i = 0; i < boardUnderTest.getCells().length; i++) {
+            for (int j = 0; j < boardUnderTest.getCells()[0].length; j++) {
+                boardUnderTest.getCells()[i][j] = new Cell(' ');
+            }
+        }
+
+        boardUnderTest.getCells()[positionUnderTest.getPosX()][positionUnderTest.getPosY()].setChip('X');
+        boardUnderTest.getCells()[positionUnderTest.getPosX() + 1][positionUnderTest.getPosY() + 1].setChip('X');
+        boardUnderTest.getCells()[positionUnderTest.getPosX() - 1][positionUnderTest.getPosY() - 1].setChip('X');
+
+        Assert.assertTrue(gameUnderTest.checkWinner(boardUnderTest));
+    }
+
+    @Test
+    public void checkWinnerWithCellsInDiagonalInverseWorks() {
+        Position positionUnderTest = mock(Position.class);
+        when(positionUnderTest.getPosX()).thenReturn(0);
+        when(positionUnderTest.getPosY()).thenReturn(2);
+
+        Board boardUnderTest = new Board();
+        Game gameUnderTest = new Game();
+
+        for (int i = 0; i < boardUnderTest.getCells().length; i++) {
+            for (int j = 0; j < boardUnderTest.getCells()[0].length; j++) {
+                boardUnderTest.getCells()[i][j] = new Cell(' ');
+            }
+        }
+
+        boardUnderTest.getCells()[positionUnderTest.getPosX()][positionUnderTest.getPosY()].setChip('X');
+        boardUnderTest.getCells()[positionUnderTest.getPosX() + 1][positionUnderTest.getPosY() - 1].setChip('X');
+        boardUnderTest.getCells()[positionUnderTest.getPosX() + 2][positionUnderTest.getPosY() - 2].setChip('X');
+
+        Assert.assertTrue(gameUnderTest.checkWinner(boardUnderTest));
+    }
+
+    @Test
+    public void checkDrawWhenThereIsNoMoreRoomForPuttingCellsWorks() {
+        Board boardUnderTest = new Board();
+        Game gameUnderTest = new Game();
+
+        for (int i = 0; i < boardUnderTest.getCells().length; i++) {
+            for (int j = 0; j < boardUnderTest.getCells()[0].length; j++) {
+                boardUnderTest.getCells()[i][j] = new Cell('?');
+            }
+        }
+
+        Assert.assertTrue(gameUnderTest.gameHasFinished(boardUnderTest));
+    }
+}
