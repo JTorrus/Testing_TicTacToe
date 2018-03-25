933d492fff4fab846f8f6cb648724f2e7e3d91bb
JTorrus <jtorrusgalindo@gmail.com>
2018-03-25 01:57:30 +0100

    Testing for BoardTest deleted, Mockito-Kotlin added to Maven dependencies

--- test/java/BoardTest.kt
+++ test/java/BoardTest.kt
@@ -1,9 +1,5 @@
+import com.nhaarman.mockito_kotlin.*
 import org.junit.Test
-import org.mockito.Mockito
 
 class BoardTest {
-    @Test
-    fun boardInitializationWorksWithCellObjects() {
-        val board = Mockito.mock(Cell::class.java)
-    }
 }
